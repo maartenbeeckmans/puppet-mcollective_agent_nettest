f60ffca1e3f0432d696b900c0ca271f3b9023deb
Maarten Beeckmans <maartenb@inuits.eu>
2021-01-23 21:23:06 +0100

    initial commit

---
+++ update_module.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+module_name='choria-mcollective_agent_nettest'
+module_version='4.0.3'
+
+echo 'Updating module'
+
+wget -c "https://forge.puppet.com/v3/files/${module_name}-${module_version}.tar.gz" -O - | tar -xz
+
+mv ${module_name}-${module_version}/* .
+
+rm -rfv "${module_name}-${module_version}"
